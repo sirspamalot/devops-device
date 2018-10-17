1fc4fc89068ab59337eaac41f812ed3a5670593d
Oliver Schneidewind <schneidewind@printf-debugging.de>
2018-10-17 22:02:35 +0200

    I would like to work with real professionals!

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 Compile from source and add to modules via 'insmod devops.ko'.
-
-Then create a new device with 'mknod /dev/ops c 222 0' and read from it
+Remove the modules via 'rmmod devops.ko'.
--- devops.c
+++ devops.c
@@ -1,28 +1,62 @@
-#include<linux/module.h>
-#include<linux/init.h>
-#include"devops.h"
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#include <linux/fs.h>
+#include <linux/kernel.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
 
-MODULE_AUTHOR("fwerner");
-MODULE_DESCRIPTION("A simple char device");
+const char devops_data[] = "Told you so!\n";
 
-static int r_init(void);
-static void r_cleanup(void);
-
-module_init(r_init);
-module_exit(r_cleanup);
-
-
-static int r_init(void)
+ssize_t devops_read(struct file *filep, char *buff,
+			size_t count, loff_t *offp)
 {
-printk("<1>hi\n");
-if(register_chrdev(222,"devops",&my_fops)){
-	printk("<1>failed to register");
+	ssize_t res = copy_to_user(buff, devops_data, strlen(devops_data));
+
+	if (unlikely(res))
+		pr_err("Kernel -> userspace copy failed!\n");
+	return res ?: strlen(devops_data);
 }
-return 0;
-}
-static void r_cleanup(void)
+
+static const struct file_operations devops_fops = {
+	.owner	= THIS_MODULE,
+	.read	= devops_read,
+};
+
+struct miscdevice devops_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "ops",
+	.fops = &devops_fops,
+	.mode = 0444,
+};
+
+static int __init devops_init(void)
 {
-printk("<1>bye\n");
-unregister_chrdev(222,"devops");
-return ;
+	int error = misc_register(&devops_device);
+
+	if (error)
+		pr_err("failed to register\n");
+	return 0;
 }
+
+static void __exit devops_exit(void)
+{
+	misc_deregister(&devops_device);
+	pr_info("Thats all folks\n");
+}
+
+module_init(devops_init)
+module_exit(devops_exit)
+
+MODULE_AUTHOR("Oliver Schneidewind");
+MODULE_DESCRIPTION("A simple example for FWerner");
+MODULE_LICENSE("GPL v2");
